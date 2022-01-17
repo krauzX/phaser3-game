ab3dfba3c19b5c62f9890c86c851394befef20c8
Failproo <aryanrajapandey@gmail.com>
2022-01-17 15:38:50 +0530

    new update

--- js/scenes/Game.js
+++ js/scenes/Game.js
@@ -1,4 +1,7 @@
- class Game extends Phaser.Scene {
+
+//import Phaser from 'phaser'
+
+class Game extends Phaser.Scene {
     constructor() {
       super('Game');
         
@@ -15,7 +18,30 @@
    
  const ball = this.add.circle(400,250,10,0xffffff,1);
  this.physics.add.existing(ball);
- ball.body.setVelocity(200,200)
-    
+    ball.body.setVelocity(-200,0);
+     ball.body.setCollideWorldBounds(true,1,1);
+    ball.body.setBounce(1, 1)
+ this.paddleLeft = this.add.rectangle(50,250,30,100,0xffffff,1)
+    this.physics.add.existing(this.paddleLeft,true)
+
+    this.physics.add.collider(this.paddleLeft , ball);
+ 
+  this.cursors =  this.input.keyboard.createCursorKeys()
+
+
+  }
+  update() {
+
+    /**@type{Phaser.Physics.Arcade.StaticBody}*/
+    const bob = this.paddleLeft.body;
+
+    if(this.cursors.up.isDown) {
+      this.paddleLeft.y -= 10
+      bob.updateFromGameObject()
+    }
+    else if (this.cursors.down.isDown) {
+      this.paddleLeft.y += 10
+      bob.updateFromGameObject()
+    }
   }
 }
