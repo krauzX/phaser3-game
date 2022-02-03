5f5414353fa4f444d1323d4c35e5accc49eda30b
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2022-02-03 06:37:24 -0500

    set the asset load path

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -1,34 +1,32 @@
 //import Phaser from './phaser'
+class Preload extends Phaser.Scene {
 
+    constructor() {
+        super('preload')
+    }
+    preload() {
+        this.add.text(200, 200, 'hi')
 
+        // Set the asset load path
+        this.load.setPath('public/assets/');
 
- class Preload extends Phaser.Scene 
-{
-  
-  constructor() {
-    super('preload')
-  }
-	preload()
-	{
-		this.add.text(200,200,'hi')
-		
-		    this.load.setPath('public/assets/');
+        
+        //this.load.audio('pong-beep', '../../public/assets/ping_pong_8bit_beeep.ogg')
+        // does NOT handle long paths containing ../../ so please avoid
+        
+        
+        this.load.audio('pong-beep', 'ping_pong_8bit_beeep.ogg')
+        this.load.audio('pong-plop', 'ping_pong_8bit_plop.ogg')
+    }
 
-		this.load.audio('pong-beep','ping_pong_8bit_beeep.ogg')
-		this.load.audio('pong-plop','ping_pong_8bit_plop.ogg')
-	}
+    create() {
+        console.log('preload');
+        
+        var beep = this.sound.add('pong-beep');
 
-	create()
-	{
-	  console.log('preload');
-//          this.sound.add('pong-beep');
-
-    var beep = this.sound.add('pong-beep');
-
-
-//this.sound.add('beep');
-	  
-	  console.log(this.cache.audio.getKeys());
-		this.scene.start('titlescreen');
-	}
+        console.log(this.cache.audio.getKeys());
+        
+        this.scene.start('titlescreen');
+        
+    }
 }
