0b04474dde977df16c3a46541aee72b813abc4f1
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2022-02-03 06:32:24 -0500

    set the asset load path

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -1,6 +1,7 @@
 //import Phaser from './phaser'
 
 
+
  class Preload extends Phaser.Scene 
 {
   
@@ -10,13 +11,24 @@
 	preload()
 	{
 		this.add.text(200,200,'hi')
-		this.load.audio('pong-beep','../../public/assets/ping_pong_8bit_beeep.ogg')
-	this.load.audio('pong-plop','../../public/assets/ping_pong_8bit_plop.ogg')
+		
+		    this.load.setPath('public/assets/');
+
+		this.load.audio('pong-beep','ping_pong_8bit_beeep.ogg')
+		this.load.audio('pong-plop','ping_pong_8bit_plop.ogg')
 	}
 
 	create()
 	{
-	  console.log('preload')
-		this.scene.start('titlescreen')
+	  console.log('preload');
+//          this.sound.add('pong-beep');
+
+    var beep = this.sound.add('pong-beep');
+
+
+//this.sound.add('beep');
+	  
+	  console.log(this.cache.audio.getKeys());
+		this.scene.start('titlescreen');
 	}
 }
